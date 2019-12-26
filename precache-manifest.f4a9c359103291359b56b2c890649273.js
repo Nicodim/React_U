a71a609e2b84b514e595fca9ea21aa1a357d5582
Николай <33102087+Nicodim@users.noreply.github.com>
2019-12-26 17:15:09 +0200

    Updates

---
+++ precache-manifest.f4a9c359103291359b56b2c890649273.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d455ce5deecbe8862ac17bb67489e0d2",
+    "url": "/React_U/index.html"
+  },
+  {
+    "revision": "37d65d8cbfb1f0c3b91f",
+    "url": "/React_U/static/css/main.535efc1e.chunk.css"
+  },
+  {
+    "revision": "ea6ad15131bc22063cba",
+    "url": "/React_U/static/js/2.b5bb128a.chunk.js"
+  },
+  {
+    "revision": "37d65d8cbfb1f0c3b91f",
+    "url": "/React_U/static/js/main.839c6322.chunk.js"
+  },
+  {
+    "revision": "1afb7038878457c11ae1",
+    "url": "/React_U/static/js/runtime-main.d2fce71b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f993271100e720691c84e373b8644d73.js"
+  "/React_U/precache-manifest.f4a9c359103291359b56b2c890649273.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_U/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
