bd5578d17a1e8861ee788f65f4a02e28cdbb4074
Николай <33102087+Nicodim@users.noreply.github.com>
2019-12-26 17:11:22 +0200

    Updates

---
+++ precache-manifest.f993271100e720691c84e373b8644d73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9552adda623762d09e3ede4d1032d3ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37d65d8cbfb1f0c3b91f",
+    "url": "/static/css/main.535efc1e.chunk.css"
+  },
+  {
+    "revision": "ea6ad15131bc22063cba",
+    "url": "/static/js/2.b5bb128a.chunk.js"
+  },
+  {
+    "revision": "37d65d8cbfb1f0c3b91f",
+    "url": "/static/js/main.839c6322.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
